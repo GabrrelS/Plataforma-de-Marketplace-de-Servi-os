5c2655978ce5a92c2a9298d24d1231490ec35644
caioelias3789 <caioelias3789@gmail.com>
2026-03-18 20:04:35 -0300

    CRUD e configuração inicial do Swagger

---
+++ src/Marketplace.API/Models/Servico.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Marketplace.API.Models
+{
+    public class Servico
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
