374ca55b65bdd6ee84642a87070fab81fb302492
Gabriel <bielsousa5514@gmail.com>
2026-06-03 17:16:31 -0300

    ci: resolvendo problemas na build dentro dos jobs

---
+++ src/PlataformaServicos/MicroServices/NotificationService/Services/NotificacaoService.cs
@@ -0,0 +1,11 @@
+namespace NotificationService.Services;
+
+public class NotificacaoService
+{
+    // O seu amigo precisa colocar a lógica de envio aqui dentro, tipo:
+    public async Task EnviarStatus(PropostaStatusDto dto)
+    {
+        // Lógica para enviar a notificação (Email, SMS, SignalR, etc.)
+        await Task.CompletedTask;
+    }
+}
